e9c1f65b6eeef1d7bf1284d06348b1fa05f98f4a
Jerome <jerome@adalong.com>
2024-02-14 12:15:38 +0100

    Connexion / Deconnexion

---
+++ ConfigurationHelper.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+
+namespace FortniteBot;
+
+public static class ConfigurationHelper
+{
+    public static string GetByName(string configKeyName)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        IConfigurationSection section = config.GetSection(configKeyName);
+
+        if (section.Value == null)
+        {
+            return "";
+        }
+
+        return section.Value;
+    }
+}
---
+++ FortniteBot.cs
@@ -0,0 +1,42 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace FortniteBot;
+
+public class FortniteBot
+{
+	private static DiscordSocketClient? discordClient;
+
+	public static async Task Main()
+	{
+
+		var token = ConfigurationHelper
+			.GetByName("Discord:Bot:Token");
+
+
+		discordClient = new DiscordSocketClient();
+		discordClient.Log += Log;
+
+		Console.CancelKeyPress += async (sender, eventArgs) =>
+        {
+            eventArgs.Cancel = true;
+			
+			await discordClient.LogoutAsync();
+            await discordClient.StopAsync();
+
+			await Task.Delay(1000);
+            Environment.Exit(0);
+        };
+
+		await discordClient.LoginAsync(TokenType.Bot, token);
+		await discordClient.StartAsync();
+
+		await Task.Delay(-1);
+	}
+
+	private static Task Log(LogMessage msg)
+	{
+		Console.WriteLine(msg.ToString());
+		return Task.CompletedTask;
+	}
+}
