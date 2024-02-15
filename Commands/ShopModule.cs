1df12cad1cc3fc87f2566ad988410ff7b109cdd1
Titou <enzofredon82@gmail.com>
2024-02-15 18:17:20 +0100

    Ajout de la boutique

---
+++ Commands/ShopModule.cs
@@ -0,0 +1,93 @@
+﻿using Discord;
+using Discord.Commands;
+using FortniteBot.Shop;
+using Newtonsoft.Json;
+
+
+namespace FortniteBot
+{
+   public class ShopModule : ModuleBase<SocketCommandContext>
+    {
+        private Embed builtEmbed;
+
+        [Command("shop")]
+        [Summary("See the shop of fortnite today")]
+        public async Task ShopCommand()
+        {
+            string apiKey = ConfigurationHelper.GetByName("Discord:API:Key");
+            string URL = ConfigurationHelper.GetByName("Discord:API:Shop:URL");
+
+            using HttpClient Client = new();
+
+            try
+            {
+                Client.DefaultRequestHeaders.Add("Authorization", apiKey);
+                HttpResponseMessage response = await Client.GetAsync($"{URL}");
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                    var responseData = JsonConvert.DeserializeObject<ShopData>(jsonResponse);
+
+                    if (responseData != null)
+                    {
+                        var embed = new EmbedBuilder
+                        {
+                            Title = $"__{responseData.Data.Featured.Entries[0].Bundle.Name}__",
+                            ImageUrl = responseData.Data.Featured.Entries[0].Bundle.Image,
+                            Description = responseData.Data.Featured.Entries[0].Bundle.Info +
+                                $"\n----------------------------------------------\n",
+                            Color = new Color(0, 255, 255),
+
+                         
+                           
+                        };
+
+                        embed.AddField("Price", $"{responseData.Data.Featured.Entries[0].RegularPrice} :moneybag:", inline: true);
+                        embed.AddField("Special Price", $"{responseData.Data.Featured.Entries[0].FinalPrice} :moneybag:", inline: true);
+
+                        //var isGiftable = responseData.Data.Featured.Entries[0].Giftable;
+                        var isGiftable = false;
+                        var giftableText = isGiftable.ToString();
+                        if (isGiftable)
+                        {
+                            giftableText = isGiftable.ToString() + " :gift:";
+                        }
+                        embed.AddField("Giftable", giftableText, inline: true);
+                        builtEmbed = embed.Build();
+
+                    }
+                    await ReplyAsync(embed: builtEmbed);
+
+
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Une erreur s'est produite : {ex.Message}");
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Data/ShopData.cs
@@ -0,0 +1,44 @@
+﻿using FortniteBot;
+
+namespace FortniteBot.Shop
+{
+    public class ShopData
+    {
+        public Data Data { get; set; }
+
+    }
+
+    public class Data
+    {
+
+        public string VbuckIcon { get; set; }
+        public Featured Featured { get; set; }
+
+    }
+
+
+    public class Featured
+    {
+        public List<Entry> Entries { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    public class Entry
+    {
+        public int RegularPrice { get; set; }
+        public int FinalPrice { get; set; }
+        public Bundle Bundle { get; set; }
+        public bool Giftable { get; set; }
+    }
+
+    public class Bundle
+    {
+        public string Info { get; set; }
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        
+    }
+}
