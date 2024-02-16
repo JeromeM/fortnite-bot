1bfc1682f53ede0eb5e6b2683e8dde745fda778b
JeromeM <meyer.jerome@gmail.com>
2024-02-16 14:52:19 +0100

    Preparation des cosmetics Ajout de l'internationalisation (EN pour le moment)

---
+++ ResourceManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Resources;
+
+namespace FortniteBot
+{
+    public class FortniteResourceManager
+    {
+        private static readonly string DEFAULT_LANGUAGE = "en";
+
+        public static string GV(string key)
+        {
+            ResourceManager rm = new ResourceManager(DEFAULT_LANGUAGE, typeof(FortniteBot).Assembly);
+            var localizedString = rm.GetString(key);
+            if (localizedString != null)
+            {
+                return localizedString;
+            }
+            return "";
+        }
+    }
+}
