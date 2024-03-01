a56e05802a6a0865f9cf5bb1837a69bbec83f842
Titou <enzofredon82@gmail.com>
2024-03-01 20:49:04 +0100

    Ajout de la fonctions code (a vrérifier)

---
+++ Data/CodeData.cs
@@ -0,0 +1,17 @@
+﻿namespace FortniteBot.Data.Code
+{
+    public class CodeData
+    {
+        public Data Data { get; set; }
+    }
+    public class Data
+    {
+        public string Code { get; set; }
+        public Account Account  { get; set; }
+        public string Statues { get; set; }
+    }
+    public class Account
+    {
+        public string Name { get; set; }
+    }
+}
